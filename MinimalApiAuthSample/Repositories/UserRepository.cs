5fcf9b437e56f104230438bf8b569391e9cf8a66
fcsantos <fcsantos.ap@gmail.com>
2021-11-07 21:27:25 -0300

    first commit

---
+++ MinimalApiAuthSample/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using MinimalApiAuthSample.Models;
+
+namespace MinimalApiAuthSample.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>
+            {
+                new User{ Id = 1, Username = "userteste1", Password = "userteste1", Role = "manager" },
+                new User{ Id = 2, Username = "userteste2", Password = "userteste2", Role = "employee" }
+            };
+
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && 
+                                    x.Password == password).FirstOrDefault();
+        }
+    }
+}
---
+++ MinimalApiAuthSample/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace MinimalApiAuthSample
+{
+    public class Settings
+    {
+        public static string Secret = "9A7BE27DC3D247169D8BD0B8C3A7147D";
+    }
+}
